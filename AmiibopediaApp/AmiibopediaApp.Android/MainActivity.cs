468a3e9b374562d7bf62a200024ab3fc956f7489
lgjoseluis <lgjoseluis@hotmail.com>
2022-03-16 19:47:37 -0600

    Agregar librería de Material UI para Xamarin Forms

--- AmiibopediaApp/AmiibopediaApp.Android/MainActivity.cs
+++ AmiibopediaApp/AmiibopediaApp.Android/MainActivity.cs
@@ -18,6 +18,8 @@ namespace AmiibopediaApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+
             LoadApplication(new App(new AndroidInitializer()));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
